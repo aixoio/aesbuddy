2ae5f624522c31e8c10dcc43bf1cdf11d3166810
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-04-20 22:10:27 +1000

    add tests for aes gcm 192 and 256

--- aes_test.go
+++ aes_test.go
@@ -38,3 +38,67 @@ func TestAesGCM128(t *testing.T) {
 	}
 
 }
+
+func TestAesGCM192(t *testing.T) {
+	key := []byte("THIS IS MY KEY!!THIS IS ")
+	data := []byte("MY SECRET MESSAGE")
+
+	enced, err := aesbuddy.AesGCMEncrpyt(key, data)
+	if err != nil {
+		t.Fatalf(err.Error())
+		t.Fail()
+		return
+	}
+
+	deced, err := aesbuddy.AesGCMDecrpyt(key, enced)
+	if err != nil {
+		t.Fatalf(err.Error())
+		t.Fail()
+		return
+	}
+
+	if slices.Compare(deced, enced) == 0 {
+		t.Log("enced == deced")
+		t.Fail()
+		return
+	}
+
+	if slices.Compare(data, deced) != 0 {
+		t.Log("data != deced")
+		t.Fail()
+		return
+	}
+
+}
+
+func TestAesGCM256(t *testing.T) {
+	key := []byte("THIS IS MY KEY!!THIS IS MY KEY!!")
+	data := []byte("MY SECRET MESSAGE")
+
+	enced, err := aesbuddy.AesGCMEncrpyt(key, data)
+	if err != nil {
+		t.Fatalf(err.Error())
+		t.Fail()
+		return
+	}
+
+	deced, err := aesbuddy.AesGCMDecrpyt(key, enced)
+	if err != nil {
+		t.Fatalf(err.Error())
+		t.Fail()
+		return
+	}
+
+	if slices.Compare(deced, enced) == 0 {
+		t.Log("enced == deced")
+		t.Fail()
+		return
+	}
+
+	if slices.Compare(data, deced) != 0 {
+		t.Log("data != deced")
+		t.Fail()
+		return
+	}
+
+}
