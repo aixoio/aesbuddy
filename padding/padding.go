f68618178d0b47c7564e01db7d4772806899fc46
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-04-26 15:45:50 +1000

    Update padding

--- padding/padding.go
+++ padding/padding.go
@@ -2,8 +2,13 @@ package padding
 
 import "bytes"
 
-func PKCS5Padding(ciphertext []byte, blockSize int, after int) []byte {
-	padding := (blockSize - len(ciphertext)%blockSize)
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
 }
