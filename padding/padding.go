390a945c3b045ddcc3e23a06d58c168e2cb70cfa
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-04-26 15:04:52 +1000

    Add padding func

---
+++ padding/padding.go
@@ -0,0 +1,9 @@
+package padding
+
+import "bytes"
+
+func PKCS5Padding(ciphertext []byte, blockSize int, after int) []byte {
+	padding := (blockSize - len(ciphertext)%blockSize)
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
